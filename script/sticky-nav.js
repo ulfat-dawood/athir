a415d868eee014572c70135c21a6d050dae1d47b
ulfat-dawood <ulfat_dawood@outlook.com>
2021-11-10 10:22:26 +0300

    acheivments

--- script/sticky-nav.js
+++ script/sticky-nav.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', ()=>{
     const nav= document.querySelector('#header');
     
-    if(window.pageYOffset > 90){
+    if(window.pageYOffset > 40){
         nav.classList.add('sticky');
 
     }else{
