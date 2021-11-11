4c78425e1bff9110dd57182061519510c7ba0d15
ulfat-dawood <ulfat_dawood@outlook.com>
2021-11-12 01:21:36 +0300

    partners carouse

--- script/carousel.js
+++ script/carousel.js
@@ -1,7 +1,24 @@
 $(".carousel").owlCarousel({
-    margin: 50,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 6000,
-    autoplayHoverPause: true, 
-}); 
+  center: true,
+  autoWidth:true,
+//   items: 4,
+  loop: true,
+  margin: 50,
+  autoplay: true,
+  slideTransition: "linear",
+  autoplayTimeout: 6000,
+  autoplaySpeed: 6000,
+//   autoplayHoverPause: true,
+  
+//   responsive: {
+//     0: {
+//       items: 1
+//     },
+//     600: {
+//       items: 3
+//     },
+//     1000: {
+//       items: 3
+//     }
+//   }
+});
