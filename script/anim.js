b38ac242d1ea3216731bfcfb72a554f44ee84e8d
ulfat-dawood <ulfat_dawood@outlook.com>
2021-11-18 12:00:22 +0300

    looping

--- script/anim.js
+++ script/anim.js
@@ -16,25 +16,25 @@ const bannerLogo = ()=>{
     .fromTo('#dashy #darkBar1', {fill:'transparent'}, {fill:"#0f3951", duration:2},"-=3")
     .fromTo('#dashy #darkBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
     .fromTo('#dashy #darkBar2', {fill:'transparent'}, {fill:"#0f3951", duration:2},"-=3")
-    .to('[id*="dark"]',0, {strokeDasharray:0},"-=.5")
+    .to('[id*="dark"]',0, {strokeDasharray:0})
 
     .fromTo('#dashy #greenBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
     .fromTo('#dashy #greenBar1', {fill:'transparent'}, {fill:"#2c6b6b", duration:3},"-=3")
     .fromTo('#dashy #greenBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
     .fromTo('#dashy #greenBar2', {fill:'transparent'}, {fill:"#2c6b6b", duration:3},"-=3")
-    .to('[id*="green"]',0, {strokeDasharray:0},"-=.5")
+    .to('[id*="green"]',0, {strokeDasharray:0})
 
     .fromTo('#dashy #blueBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
     .fromTo('#dashy #blueBar1', {fill:'transparent'}, {fill:"#1a81c6", duration:2},"-=3")
     .fromTo('#dashy #blueBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
     .fromTo('#dashy #blueBar2', {fill:'transparent'}, {fill:"#1a81c6", duration:2},"-=3")
-    .to('[id*="blue"]',0, {strokeDasharray:0},"-=.5")
+    .to('[id*="blue"]',0, {strokeDasharray:0})
 
     .fromTo('#dashy #yellowBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
     .fromTo('#dashy #yellowBar1', {fill:'transparent'}, {fill:"#e5901f", duration:3},"-=3")
     .fromTo('#dashy #yellowBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
     .fromTo('#dashy #yellowBar2', {fill:'transparent'}, {fill:"#e5901f", duration:3},"-=3")
-    .to('[id*="yellow"]',0, {strokeDasharray:0},"-=.5")
+    .to('[id*="yellow"]',0, {strokeDasharray:0})
 
 
 
