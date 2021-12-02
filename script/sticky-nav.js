9c87a5dfd9a414d5c0cb8783df77e87bcb8dba0e
ulfat-dawood <ulfat_dawood@outlook.com>
2021-12-02 12:56:08 +0300

    nav added

--- script/sticky-nav.js
+++ script/sticky-nav.js
@@ -8,3 +8,18 @@ window.addEventListener('scroll', ()=>{
         nav.classList.remove('sticky');
     }
 }); 
+
+
+// responsive navbar 
+
+const burger = document.querySelector('#menu-icon'); 
+const sidebar = document.querySelector('#sidebar'); 
+
+burger.addEventListener('click', _=>{
+    if(!sidebar.classList.contains("show")){
+        console.log('clickyyy'); 
+        sidebar.classList.add("show"); 
+    }else{
+        sidebar.classList.remove("show");
+    }
+})
