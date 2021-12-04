f7b20dd42483f373d6ef5cad47540e20e1e671cf
ulfat-dawood <ulfat_dawood@outlook.com>
2021-12-04 19:21:54 +0300

    chart animation

--- script/anim.js
+++ script/anim.js
@@ -1,9 +1,9 @@
 
 
-// document.querySelector('#header').addEventListener('click',()=>{
+document.querySelector('#header').addEventListener('click',()=>{
    
-//     bannerLogo(); 
-// })
+    dashboardAnim(); 
+})
 
 const bannerLogo = ()=>{
 
@@ -35,6 +35,46 @@ const bannerLogo = ()=>{
     .fromTo('#dashy #yellowBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
     .fromTo('#dashy #yellowBar2', {fill:'transparent'}, {fill:"#e5901f", duration:3},"-=3")
     .to('[id*="yellow"]',0, {strokeDasharray:0})
+
+
+
+}
+
+
+
+
+const dashboardAnim = ()=>{
+
+    let timeline1 = new TimelineMax();
+
+    timeline1
+    .from('#baz .bar', {scaleY:0, transformOrigin : "center bottom", stagger: .2})
+    .fromTo('#baz #chart #line' ,2,{ strokeDasharray: 90, strokeDashoffset:90}, {strokeDashoffset:0 ,duration:2, ease: "expo.out"})
+    .fromTo("#baz #chart circle", {opacity: 0}, {opacity:1 ,duration:2, stagger: .2} , "<" )
+
+    // .fromTo('#dashy #darkBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"<")
+    // .fromTo('#dashy #darkBar1', {fill:'transparent'}, {fill:"#0f3951", duration:2},"-=3")
+    // .fromTo('#dashy #darkBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
+    // .fromTo('#dashy #darkBar2', {fill:'transparent'}, {fill:"#0f3951", duration:2},"-=3")
+    // .to('[id*="dark"]',0, {strokeDasharray:0})
+
+    // .fromTo('#dashy #greenBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
+    // .fromTo('#dashy #greenBar1', {fill:'transparent'}, {fill:"#2c6b6b", duration:3},"-=3")
+    // .fromTo('#dashy #greenBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
+    // .fromTo('#dashy #greenBar2', {fill:'transparent'}, {fill:"#2c6b6b", duration:3},"-=3")
+    // .to('[id*="green"]',0, {strokeDasharray:0})
+
+    // .fromTo('#dashy #blueBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
+    // .fromTo('#dashy #blueBar1', {fill:'transparent'}, {fill:"#1a81c6", duration:2},"-=3")
+    // .fromTo('#dashy #blueBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:3},"-=2")
+    // .fromTo('#dashy #blueBar2', {fill:'transparent'}, {fill:"#1a81c6", duration:2},"-=3")
+    // .to('[id*="blue"]',0, {strokeDasharray:0})
+
+    // .fromTo('#dashy #yellowBar1', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
+    // .fromTo('#dashy #yellowBar1', {fill:'transparent'}, {fill:"#e5901f", duration:3},"-=3")
+    // .fromTo('#dashy #yellowBar2', {strokeDashoffset:200},{strokeDashoffset:0, duration:2},"-=2")
+    // .fromTo('#dashy #yellowBar2', {fill:'transparent'}, {fill:"#e5901f", duration:3},"-=3")
+    // .to('[id*="yellow"]',0, {strokeDasharray:0})
 
 
 
