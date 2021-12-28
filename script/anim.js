0d7df4de83275fd54fa5f759c78e9468c22e32cd
ulfat-dawood <ulfat_dawood@outlook.com>
2021-12-28 17:52:06 +0300

    splash animation done

--- script/anim.js
+++ script/anim.js
@@ -1,7 +1,32 @@
 
+window.addEventListener('load', ()=>{
+    document.querySelector('#splash .logo-container').classList.remove("logo-anim")
+    splashAnim();
+    setTimeout(()=>{
+        bannerLogo();
+    },1000) 
+});
+
+
+//SPLASH SCREEN
+const splashAnim = ()=>{
+    let splashTimeline= new TimelineMax();
+    const oldBg= "linear-gradient(45deg, rgba(12,45,65,1) 40%, rgba(15,57,81,1) 100%)";
+    splashTimeline
+    //logo zoom in
+    .to("#splash .logo-container", {scale:10,rotation: 45,opacity:0, duration: 1})
+    //bg to white
+    .fromTo("#splash",{background:oldBg}, {background:"white", duration: 1.5},"<")
+    //splash opacity=0 w=0 h=0
+    .to("#splash", {opacity:0, width:0, height:0, duration: .1},"-=.5")
+    .to("#splash", { width:0, height:0, duration: 0},"<1");
+
+}
+  
 
 
 
+//BANNER LOGO
 const bannerLogo = ()=>{
 
     let timeline = new TimelineMax();
@@ -39,7 +64,7 @@ const bannerLogo = ()=>{
 
 
 
-
+//DASHBOARD
 const dashboardAnim = ()=>{
     const piePath1= "M27.65,30.45,0,17.69A30.45,30.45,0,0,1,58.1,30.45Z"; 
     const piePath= "M88.17,93.08,60.74,79.86a30.45,30.45,0,0,1,57.88,13.22Z"; 
@@ -62,36 +87,3 @@ const dashboardAnim = ()=>{
 
 
 
-//SPLASH SCREEN
-document.querySelector('#splash').addEventListener('click',()=>{
-   
-    dashboardAnim(); 
-})
-
-window.addEventListener('load', ()=>{
-    bannerLogo();
-    // setTimeout(_=> window.location.hash = '#banner2',10000)
-})
-
-const splashAnim = ()=>{
-let splashTimeline= new TimelineMax();
-
-splashTimeline
-// Bars 
-.from('#baz .bar', {scaleY:0, transformOrigin : "center bottom", stagger: .2})
-.fromTo('#baz #chart #line' ,2,{ strokeDasharray: 90, strokeDashoffset:90}, {strokeDashoffset:0 ,duration:2, ease: "expo.out"})
-.fromTo("#baz #chart circle", {scale: 0}, {scale:1 ,duration:2, stagger: .2} , "<" )
-
-//bullet points:
-.fromTo("#baz #list .dot", {y:10,opacity:0}, {y:0, opacity:1,duration:1, stagger: .3},1)
-.from("#baz #list .li" ,  {scaleY:0, transformOrigin : "center bottom", stagger: .2},"<")
-.fromTo("#baz #box .boxy" , {y:-5, opacity:0}, {y:10, opacity:1, stagger: .2},"<")
-
-}
-  
-  //1-continous logo amination
-  //////ON-LOADING://////
-  //2-stop the logo aniamtion
-  //3-logo bg 2 white + bg 2 whit + logo zoom in
-  //4-splash opacity 0
-  //5-banner animation
